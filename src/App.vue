f35020bb57f33cd689af0798f112f7600301057c
nordast <nordast@gmail.com>
2024-11-21 14:04:14 +0200

    header

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
-<script setup></script>
+<script setup>
+import TheHeader from './components/TheHeader.vue'
+</script>
 
 <template>
-  <div class="">App</div>
+  <div class="bg-white w-3/5 m-auto rounded-xl shadow-xl shadow-grey-200 mt-20">
+    <TheHeader />
+  </div>
 </template>
 
 <style scoped></style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <header class="flex justify-between border-b border-slate-200 px-10 py-8">
+    <div class="flex items-center gap-4">
+      <img src="/logo.svg" alt="Logo" class="w-40" />
+    </div>
+
+    <ul class="flex items-center gap-10">
+      <li class="flex items-center gap-3 cursor-pointer">
+        <img src="/cart.svg" alt="Cart" />
+        <span class="text-gray-500 hover:text-black transition-colors">$0.00</span>
+      </li>
+      <li class="flex items-center gap-3 cursor-pointer">
+        <img src="/heart.svg" alt="Favorite" />
+        <span class="text-gray-500 hover:text-black transition-colors">Favorite</span>
+      </li>
+      <li class="flex items-center gap-3 cursor-pointer">
+        <img src="/profile.svg" alt="Profile" />
+        <span class="text-gray-500 hover:text-black transition-colors">Profile</span>
+      </li>
+    </ul>
+  </header>
+</template>
+<script setup lang="ts"></script>
